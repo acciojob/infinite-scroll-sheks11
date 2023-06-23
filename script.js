3cadc1115de92599d92554ed2f41d2d0cb02eb8d
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-23 15:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
-
+let list = document.querySelector('#infi-list');
+list.addEventListener('scroll', () => {
+  let rect = list.getBoundingClientRect();
+  //console.log(list.children.length);
+  if (rect.bottom <= window.innerHeight) {
+    let newItem1 = document.createElement('li');
+    newItem1.textContent = `${list.lastElementChild.textContent.substring(0,4)} ${list.children.length +1}`;
+    list.appendChild(newItem1);
+    let newItem2 = document.createElement('li');
+    newItem2.textContent = `${list.lastElementChild.textContent.substring(0,4)} ${list.children.length +1}`;
+    list.appendChild(newItem2);
+  }
+});
